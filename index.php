8f3b9000e32b4e4fa458dafe78f00d486e5ad969
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-02-05 11:47:24 +0700

    category

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+    require_once("functions/db.php");
+    $conn = ketnoidb();
+    $sql = "select * from products order by id desc limit 8";
+    $rs = $conn->query($sql);
+    $products = [];
+    while($row= $rs->fetch_assoc()){
+        $products[] = $row;
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,6 +38,22 @@
             </button>
         </div>
     </div>
-    
+    <div class="container">
+        <h2>Sản phẩm mới</h2>
+        <div class="row">
+            <?php  foreach($products as $item):?>
+            <div class="col-3 mt-3">
+                <div class="card">
+                    <img src="<?php echo $item["image"];?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $item["name"];?></h5>
+                        <p class="card-text">$<?php echo round($item["price"]);?></p>
+                        <a href="/product.php?id=<?php echo $item["id"]; ?>" class="btn btn-primary">Chi tiết</a>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach;?>
+        </div>
+    </div>
 </body>
 </html>
