a781c4c70e589cc61ce8226031c18e598edb9fe2
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-02-07 08:44:27 +0700

    search

--- html/nav.php
+++ html/nav.php
@@ -27,8 +27,8 @@
             </li>
         <?php endforeach;?>    
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <form action="/search.php" method="GET" class="d-flex" role="search">
+        <input name="q" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
---
+++ search.php
@@ -0,0 +1,45 @@
+<?php
+    $q = $_GET['q'];
+    require_once("functions/db.php");
+    $conn = ketnoidb();
+    $sql = "select * from products where name like '%$q%' order by id desc";
+    $rs = $conn->query($sql);
+    $products = [];
+    while($row= $rs->fetch_assoc()){
+        $products[] = $row;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+    <?php include_once("html/head.php");?>
+</head>
+<body>
+    <?php include_once("html/nav.php");?>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <h2>Left sidebar</h2>
+            </div>
+            <div class="col">
+                <div class="row">
+                <?php  foreach($products as $item):?>
+                    <div class="col-4 mt-3">
+                        <div class="card">
+                            <img src="<?php echo $item["image"];?>" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $item["name"];?></h5>
+                                <p class="card-text">$<?php echo round($item["price"]);?></p>
+                                <a href="/product.php?id=<?php echo $item["id"]; ?>" class="btn btn-primary">Chi tiết</a>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach;?>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
