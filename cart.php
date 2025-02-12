126d1e2bcc11b5d4e7be02264504775f700bbcb7
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-02-12 08:39:33 +0700

    cart - checkout

--- cart.php
+++ cart.php
@@ -15,6 +15,7 @@
     while($row= $rs->fetch_assoc()){
         $products[] = $row;
     }
+    $total = 0;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,20 +32,31 @@
                 <th>Image</th>
                 <th>Name</th>
                 <th>Buy QTY</th>
+                <th>Price</th>
+                <th>Total</th>
             </thead>
             <tbody>
                 <?php foreach($products as $item):?>
+                    <?php $total +=  $cart[$item["id"]] * $item["price"];?>
                    <tr>
                         <td><?php echo $item["id"];?></td>
                         <td><img width="100" src="<?php echo $item["image"];?>"/></td>
                         <td><?php echo $item["name"];?></td>
                         <td><?php echo $cart[$item["id"]];?></td>
-                        
+                        <td><?php echo $item["price"];?></td>
+                        <td><?php echo $cart[$item["id"]] * $item["price"];?></td>
+                    
                    </tr>     
                 <?php endforeach;?>    
             </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="5" class="text-end">Subtotal:</td>
+                    <td><?php echo $total;?></td>
+                </tr>
+            </tfoot>
         </table>
-    
+        <a href="/checkout.php"class="btn btn-danger">Go to checkout</a>
     </div>
     
     
---
+++ checkout.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once("html/head.php");?>
+</head>
+<body>
+    <?php include_once("html/nav.php");?>
+    <div class="container">
+        <div class="col-8">
+            <form action="#" method="post">
+
+            </form>
+        </div>
+        <div class="col">
+            <h2>Cart summary</h2>
+            
+        </div>
+    </div>
+</body>
+</html>
