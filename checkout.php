1478f57f92c202268a8b96fe5a8676d19b877856
quanghoatrinh <quanghoa.trinh@gmail.com>
2025-02-12 09:28:44 +0700

    checkout 3

--- checkout.php
+++ checkout.php
@@ -1,3 +1,22 @@
+<?php
+    session_start();
+    if(isset($_SESSION["cart"])){
+        $cart = $_SESSION["cart"];
+    }else{
+        $cart = [];
+    }
+    $ids = array_keys($cart); // [1,5,4]
+    $ids = implode(",",$ids); //[1,5,4] =>  "1,5,4"
+    require_once("functions/db.php");
+    $conn = ketnoidb();
+    $sql = "select * from products where id in ($ids)";
+    $rs = $conn->query($sql);
+    $products = [];
+    while($row= $rs->fetch_assoc()){
+        $products[] = $row;
+    }
+    $total = 0;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -26,24 +45,43 @@
                         <div class="col-12 mb-3">
                             <h4>Payment method</h4>
                             <div class="form-check">
-                                <input class="form-check-input" value="COD" type="radio" name="payment_method" id="flexRadioDefault1">
+                                <input class="form-check-input" value="COD" type="radio" name="payment_method" id="flexRadioDefault1" checked>
                                 <label class="form-check-label" for="flexRadioDefault1">
                                     COD
                                 </label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" value="PAYPAL" type="radio" name="payment_method" id="flexRadioDefault2" checked>
+                                <input class="form-check-input" value="PAYPAL" type="radio" name="payment_method" id="flexRadioDefault2" >
                                 <label class="form-check-label" for="flexRadioDefault2">
                                     Paypal
                                 </label>
                             </div>
                         </div>
+                        <div class="col-12 text-end">
+                            <button type="submit" class="btn btn-danger">Place order</button>
+                        </div>
+                      
                     </div>
                 </form>
             </div>
             <div class="col">
-                <h2>Cart summary</h2>
-
+                <h4>Cart summary</h4>
+                <ul>
+                <?php foreach($products as $item):?>
+                    <?php $total +=  $cart[$item["id"]] * $item["price"];?>
+                    <li>
+                        <img width="50" src="<?php echo $item["image"];?>"/>
+                        <?php echo $item["name"];?>
+                        ($<?php echo $item["price"];?> x <?php echo $cart[$item["id"]];?>)
+                        <span>
+                            ===> $<?php echo $cart[$item["id"]] * $item["price"];?>
+                        </span>
+                    </li>
+                <?php endforeach;?>    
+                <li>Subtotal: $<?php echo $total; ?></li>
+                <li>Shipping fee: $10</li>
+                <li><h4>Grand total:  $<?php echo $total + 10; ?></h4></li>
+                </ul>
             </div>
         </div>
     </div>
